7037d69cbbd06633cb08fa3d260c3332b6de3e74
Mecronorm <alessandropaap@hotmail.com>
2023-11-09 16:46:18 +0100

    Make it so you can see the cards when clicked

--- memory/script.js
+++ memory/script.js
@@ -1,11 +1,26 @@
-const cards = ["1","2","3","4","5","6","7","8","9","10"]
+const cards = [
+    {
+        name: 1,
+        image: "./images/Grass_Block_29_JE2_BE2.webp"
+    },
+    {
+        name: 2,
+        image: "./images/Mycelium_29_JE2_BE2.webp"
+    }
+]
 const cardsPairs = []
 const points = 0
 cards.forEach(card => {
     const original = document.createElement("button")
     const pair = document.createElement("button")
-    original.className = card
-    pair.className = card
+    const image = document.createElement("img")
+    const pairImage = document.createElement("img")
+    image.src = card.image
+    pairImage.src = card.image
+    original.className = card.name
+    pair.className = card.name
+    original.append(image)
+    pair.append(pairImage)
     cardsPairs.push(original, pair)
 });
 
@@ -15,18 +30,25 @@ cardsPairs.forEach(card =>{
             console.log(card.className);
             return
         } else {
+            if (document.body.children[1].getElementsByClassName("is-clicked").length === 2) {
+                return
+            }
             card.className = card.className + " is-clicked"
-        }
-        if (document.body.children[1].getElementsByClassName("is-clicked").length === 2) {
-            if (document.body.children[1].getElementsByClassName("is-clicked")[0].className === document.body.children[1].getElementsByClassName("is-clicked")[1].className){
-                document.body.children[1].getElementsByClassName("is-clicked")[0].classList.add("is-correct")
-                document.body.children[1].getElementsByClassName("is-clicked")[1].classList.add("is-correct")
-                document.body.children[1].getElementsByClassName("is-clicked")[0].classList.remove("is-clicked")
-                document.body.children[1].getElementsByClassName("is-clicked")[0].classList.remove("is-clicked")
-                points++
-            } else {
-                document.body.children[1].getElementsByClassName("is-clicked")[0].classList.remove("is-clicked")
-                document.body.children[1].getElementsByClassName("is-clicked")[0].classList.remove("is-clicked")
+            if (document.body.children[1].getElementsByClassName("is-clicked").length === 2) {
+                if (document.body.children[1].getElementsByClassName("is-clicked")[0].className === document.body.children[1].getElementsByClassName("is-clicked")[1].className){
+                    document.body.children[1].getElementsByClassName("is-clicked")[0].classList.add("is-correct")
+                    document.body.children[1].getElementsByClassName("is-clicked")[1].classList.add("is-correct")
+                    document.body.children[1].getElementsByClassName("is-clicked")[0].classList.remove("is-clicked")
+                    document.body.children[1].getElementsByClassName("is-clicked")[0].classList.remove("is-clicked")
+                    points++
+                } else {
+                    setTimeout(name =>{
+                        document.body.children[1].getElementsByClassName("is-clicked")[0].classList.remove("is-clicked")
+                        document.body.children[1].getElementsByClassName("is-clicked")[0].classList.remove("is-clicked")
+                    }, 1500)
+                }
+            } else{
+                
             }
         }
     })
