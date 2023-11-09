1b92a63c0ffa6ffe1bce9fda430c798822b2077b
Mecronorm <alessandropaap@hotmail.com>
2023-11-09 11:13:54 +0100

    Make dealer function

--- Blackjack/script.js
+++ Blackjack/script.js
@@ -1,6 +1,7 @@
 let suits = ["spades", "diamonds", "clubs", "hearts"]
 let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 const hitButton = document.getElementById("hit")
+const standButton = document.getElementById("stand")
 const playerHand = document.getElementById("player-hand")
 const dealerHand = document.getElementById("dealer-hand")
 
@@ -12,8 +13,9 @@ suits.forEach(emblem => {
         });
 });
 let playerTotal = 0;
+let dealerTotal = 0;
 
-function drawCard(hand){
+function drawCard(hand, total){
     const randomCard = Math.floor(Math.random()*deck.length)
     const drawnCard = deck[randomCard]
     const articleCard = document.createElement("article")
@@ -31,27 +33,74 @@ function drawCard(hand){
     articleCard.append(number)
     const emblem = document.createElement("p")
     emblem.textContent = drawnCard.suit
+    articleCard.className = drawnCard.suit
     articleCard.append(emblem)
     hand.append(articleCard)
-    playerTotal = playerTotal + +number.classList[0]
-    if (playerTotal > 21) {
+    total = total + +number.classList[0]
+    console.log(total);
+    deck.splice(randomCard, 1)
+    if (total > 21) {
         if (hand.getElementsByClassName("11")[0]){
             document.getElementsByClassName("11")[0].classList = "1"
-            playerTotal = playerTotal - 10;
-        } else {
-            const bustMessage = document.createElement("p")
-            bustMessage.textContent = "you bust"
-            hand.append(bustMessage)
+            total = total - 10;
         }
     }
-    console.log(playerTotal);
+    return total
 }
-drawCard(playerHand)
-drawCard(playerHand)
-drawCard(dealerHand)
+function dealersTurn() {
+    if (playerTotal <= 21) {
+        if (dealerTotal < 15) {
+            dealerTotal = drawCard(dealerHand, dealerTotal)
+            dealersTurn()
+            return
+        } else {
+            if (playerTotal > dealerTotal || dealerTotal > 21) {
+                const playerWin = document.createElement("p")
+                playerWin.className = "win"
+                playerWin.textContent = "Congratulations! You win!"
+                document.body.children[2].append(playerWin)
+            } else {
+                if (playerTotal === dealerTotal) {
+                    const playerDraw = document.createElement("p")
+                    playerDraw.className = "draw"
+                    playerDraw.textContent = "It's a draw!"
+                    document.body.children[2].append(playerDraw)
+                } else{
+                    const playerLose = document.createElement("p")
+                    playerLose.className = "lose"
+                    playerLose.textContent = "You lost against the dealer"
+                    document.body.children[2].append(playerLose)
+                }
+            }
+        }
+    } else {
+        const playerLose21 = document.createElement("p")
+        playerLose21.className = "lose21"
+        playerLose21.textContent = "You Bust and lost!"
+        document.body.children[2].append(playerLose21)
+    }
+    const playAgain = document.createElement("button")
+    playAgain.addEventListener("click", (event)=>{
+        location.reload()
+    })
+    playAgain.className = "play-again"
+    playAgain.textContent = "Play Again?"
+    document.body.children[2].append(playAgain)
+}
+playerTotal = drawCard(playerHand, playerTotal)
+playerTotal = drawCard(playerHand, playerTotal)
+dealerTotal = drawCard(dealerHand, dealerTotal)
 
 hitButton.addEventListener("click", (event)=>{
-    drawCard(playerHand)
+    playerTotal = drawCard(playerHand, playerTotal)
+    if (playerTotal > 21) {
+        hitButton.style.display = "none"
+        standButton.style.display = "none"
+        dealersTurn()
+    }
 })
-
-console.log(deck)
+standButton.addEventListener("click", (event) =>{
+    hitButton.style.display = "none"
+    standButton.style.display = "none"
+    dealersTurn()
+})
