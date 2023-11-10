9a47e271eef8b9d23559af763852fb73023c9f3a
mecronorm <alessandropaap@hotmail.com>
2023-11-10 13:31:25 +0100

    Add styling to the elements

--- memory/script.js
+++ memory/script.js
@@ -40,7 +40,11 @@ const cards = [
         image: "./images/steve.jpg"
     }
 ]
+const infoButton = document.getElementById("info")
+const infoArticle = document.getElementById("info-article")
+const playAgain = document.getElementById("play-again")
 const cardsPairs = []
+let tries = 0
 let points = 0
 cards.forEach(card => {
     const original = document.createElement("button")
@@ -55,7 +59,32 @@ cards.forEach(card => {
     pair.append(pairImage)
     cardsPairs.push(original, pair)
 });
-
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex > 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+shuffle(cardsPairs)  
+infoButton.addEventListener("click", (event)=> {
+    event.target.classList.toggle("active")
+    if (event.target.classList.contains("active")) {
+        cardsPairs.forEach(card =>{
+            card.style.display = "none"
+        })
+        infoArticle.style.display = "block"
+    } else {
+        if (points != cardsPairs.length / 2) {  
+            cardsPairs.forEach(card => {
+                card.style.display = "block"
+            })
+        }
+        infoArticle.style.display = "none"
+    }
+})
 cardsPairs.forEach(card =>{
     card.addEventListener("click", (event) =>{
         if (card.className.includes("is-clicked") || card.className.includes("is-correct")) {
@@ -66,6 +95,7 @@ cardsPairs.forEach(card =>{
                 return
             }
             card.className = card.className + " is-clicked"
+            tries++
             if (document.body.children[1].getElementsByClassName("is-clicked").length === 2) {
                 if (document.body.children[1].getElementsByClassName("is-clicked")[0].className === document.body.children[1].getElementsByClassName("is-clicked")[1].className){
                     document.body.children[1].getElementsByClassName("is-clicked")[0].classList.add("is-correct")
@@ -80,10 +110,22 @@ cardsPairs.forEach(card =>{
                         document.body.children[1].getElementsByClassName("is-clicked")[0].classList.remove("is-clicked")
                     }, 1500)
                 }
-            } else{
-                
             }
         }
+        if (points === cardsPairs.length / 2) {
+            setTimeout(name => {
+                cardsPairs.forEach(card => {
+                    card.style.display = "none"
+                });
+                const winMessage = document.createElement("h3")
+                winMessage.textContent = `You won, and it only took you ${tries / 2} tries!`
+                document.body.children[2].insertBefore(winMessage, document.body.children[2].firstChild)
+                document.body.children[2].style.display = "block"
+            }, 750)
+        }
     })
-    document.body.children[1].append(card)
+    document.body.children[1].children[0].append(card)
+})
+playAgain.addEventListener("click", (event)=>{
+    location.reload()
 })
